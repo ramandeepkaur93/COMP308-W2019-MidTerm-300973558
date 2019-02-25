ac7994372eda080ed2823ea880671ed5ab156132
ramandeep <ramandeepkaur9082@gmail.com>
2019-02-24 21:28:21 -0500

    Second

--- server/routes/books.js
+++ server/routes/books.js
@@ -26,9 +26,6 @@ router.get('/', (req, res, next) => {
 //  GET the Book Details page in order to add a new Book
 router.get('/add', (req, res, next) => {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
      res.render('books/details', {
         title: 'Add Books',
         books: new book(),
@@ -39,9 +36,6 @@ router.get('/add', (req, res, next) => {
 // POST process the Book Details page and create a new Book - CREATE
 router.post('/add', (req, res, next) => {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
      let newBook = new book();
      newBook.Title = req.body.title;
      newBook.Price = req.body.price;
@@ -61,9 +55,7 @@ router.post('/add', (req, res, next) => {
 // GET the Book Details page in order to edit an existing Book
 router.get('/:id', (req, res, next) => {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+
      book.findById(req.params.id, (err, books) => {
        if (err) {
          return console.error(err);
@@ -81,31 +73,31 @@ router.get('/:id', (req, res, next) => {
 // POST - process the information passed from the details form and update the document
 router.post('/:id', (req, res, next) => {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
-     let newBook = new book();
-     newBook.Title = req.body.title;
-     newBook.Price = req.body.price;
-     newBook.Author = req.body.author;
-     newBook.Genre = req.body.genre;
-     newBook.Description = "";
-     newBook.save(function(err) {
-      if (err) {
-        return console.error(err.message);
-        return res.redirect('/books/add');
-      }
-      return res.redirect('/books');
-    }); 
-
+book.findById(req.params.id, (err, book) => {
+       if (err) {
+         return console.error(err);
+       }
+       else {
+         book.Title = req.body.title;
+         book.Price = req.body.price;
+         book.Author = req.body.author;
+         book.Genre = req.body.genre;
+         book.Description = "";
+         book.save(function(err) {
+          if (err) {
+            return console.error(err.message);
+            return res.redirect('/'+req.params.id);
+          }
+          return res.redirect('/books');
+        }); 
+       }
+     });
+     //newBook._id = req.params.id;
 });
 
 // GET - process the delete by user id
 router.get('/delete/:id', (req, res, next) => {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
       book.findByIdAndRemove(req.params.id, (err, books) => {
        if (err) {
          return console.error(err);
